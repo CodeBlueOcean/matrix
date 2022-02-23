ae932ee5d19a6c42e0cf4650573b98e75040120f
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-23 00:22:59 -0600

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,17 @@
+# Matrix (this is important as for machine learning the computer can't tell this is an image or not)
+matrix = [
+    [1,2,3],
+    [2,4,6],
+    [7,8,9]
+]
+# For of example of x, in the letter Matrix (1 is green in the letter, 0 is dark) 
+matrix = [
+    [1,0,1],
+    [0,1,0],
+    [1,0,1]
+]
+
+print(matrix[0][5])
+
+#mult-dimension array (get the other bracket of the array in the matrix and then the [1,0,1] bracket of the array)
+
